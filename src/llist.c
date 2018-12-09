48286c3a0f3c0f41acefb859592e4461dd9aedfa
Ashley-P <ashleyp1244@hotmail.co.uk>
2018-12-09 15:16:45 +0000

    Implemented linked lists

---
+++ src/llist.c
@@ -0,0 +1,75 @@
+#include "llist.h"
+
+
+struct ListNode *ll_create_node(void *data) {
+    struct ListNode *node = (struct ListNode *)malloc(sizeof(struct ListNode));
+    node->next = NULL;
+    node->data = data;
+
+    return node;
+}
+
+/* Inserts a new node after the one provided */
+void ll_insert(struct ListNode *prev_node, void *data) {
+    struct ListNode *new_node = ll_create_node(data);
+
+    /* Making the pointers point to the correct nodes */
+    new_node->next  = prev_node->next;
+    prev_node->next = new_node;
+}
+
+/* frees current_node and keeps the list intact */
+void ll_delete(struct ListNode *prev_node, struct ListNode *current_node) {
+    /* Making the pointers point to the correct nodes */
+    prev_node->next = current_node->next;
+    
+    free(current_node);
+}
+
+void ll_push_front(struct ListNode **head, void *data) {
+    struct ListNode *new_node = ll_create_node(data);
+
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void ll_push_back(struct ListNode **head, void *data) {
+    struct ListNode *new_node = ll_create_node(data);
+
+    /* Checking the "next" pointer to see if it's the end of the list */
+    struct ListNode *check_node = *head;
+    while(check_node->next != NULL)
+        check_node = check_node->next;
+
+    check_node->next = new_node;
+}
+
+struct ListNode *ll_pop_front(struct ListNode **head) {
+    /* Pointing to the node about to be removed from the list */
+    struct ListNode *popped_node = *head;
+
+    /* Making the head point to the next node down the line */
+    *head = (*head)->next;
+
+    return popped_node;
+}
+
+struct ListNode *ll_pop_back(struct ListNode **head) {
+    /* If the list is one node long then just call ll_pop_front */
+    if ((*head)->next == NULL)
+        return ll_pop_front(head);
+
+    /* Setup for the loop */
+    struct ListNode *prev_node = *head;
+    struct ListNode *popped_node = (*head)->next;
+
+    /* Moving to the end of the list */
+    while (popped_node->next != NULL) {
+        prev_node = prev_node->next;
+        popped_node = popped_node->next;
+    }
+
+    /* Making a new end of the list and returning the former end */
+    prev_node->next = NULL;
+    return popped_node;
+}
---
+++ src/llist.h
@@ -0,0 +1,37 @@
+#ifndef LLIST_H
+#define LLIST_H
+
+#include <stdlib.h>
+
+
+/**
+ * The purpose of this file is to manage general purpose linked lists
+ * Requires you to keep track of nodes if you want to insert and delete at certain points
+ *
+ * Can also be treated as a deque (and a stack and queue by extension) using the other functions:
+ * ll_pop_front and ll_push_front for stacks
+ * ll_pop_front and ll_push_back for queues
+ *
+ * Head pointers should point to the front/top
+ */
+
+struct ListNode {
+    struct ListNode *next;
+    void *data;
+};
+
+/* Externs */
+/* Used internally by the other functions, but should be used for first creating the list */
+struct ListNode *ll_create_node(void *data);
+
+/* Main functions for the linked lists */
+void ll_insert(struct ListNode *node, void *data);
+void ll_delete(struct ListNode *prev_node, struct ListNode *current_node);
+
+/* Functions to treat the list as a queue or stack */
+void ll_push_front(struct ListNode **head, void *data);
+void ll_push_back(struct ListNode **head, void *data);
+struct ListNode *ll_pop_front(struct ListNode **head);
+struct ListNode *ll_pop_back(struct ListNode **head);
+
+#endif
